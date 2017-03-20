c83fce9ddf0522740715e6c99c6d44e5ff98ef54
Colossal Hendrix <vikie95@gmail.com>
2017-03-20 17:58:33 +0100

    serving static assets immediately app is initialized

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-process.env.PWD = process.cwd();
-
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var Parse = require('parse/node');
@@ -47,6 +45,12 @@ var dashboard = new ParseDashboard({
 
 var app = express();
 
+// Serve static assets from the /public folder
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Serve static assets from the /public folder
+app.use('/docs', express.static(path.join(__dirname, '/apidocs')));
+
 // Use morgan for logging and bodyParser to dissect request body
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -54,12 +58,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Mount routes
 app.use(constantsConfig.API_V1_ROUTE_PREFIX + 'users', apiv1RoutesConfig.usersRoutes);
-
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(process.env.PWD, '/public')));
-
-// Serve static assets from the /public folder
-app.use('/docs', express.static(path.join(process.env.PWD, '/apidocs')));
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
